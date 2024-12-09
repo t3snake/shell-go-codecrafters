b484ab8138d878939bef9b884502db923c108661
t3snake <t3snake@gmail.com>
2024-12-09 13:19:58 +0530

    handle invalid commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,16 +3,37 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
+	allowed_prompts := make([]string, 0)
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	prompt, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command := strings.Split(strings.Split(prompt, "\n")[0], " ")[0]
+
+	found := false
+
+	for i := 0; i < len(allowed_prompts); i++ {
+		if allowed_prompts[i] == command {
+			// TODO
+			found = true
+		}
+	}
+
+	if !found {
+		fmt.Printf("%s: not found\n", command)
+	}
 }
