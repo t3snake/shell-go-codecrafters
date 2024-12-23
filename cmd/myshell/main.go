9dce5ad6e77fe582c8e11b9065aced8101ed5cbc
t3snake <t3snake@gmail.com>
2024-12-23 23:45:03 +0530

    implement type for commands available in PATH dirs

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -21,6 +21,31 @@ func isValidCommand(command string, allowed []string) bool {
 	return false
 }
 
+func isInPath(command string) string {
+	path := os.Getenv("PATH")
+
+	directories := strings.Split(path, ":")
+
+	for i := 0; i < len(directories); i++ {
+		// fmt.Println(directories[i])
+		files, err := os.ReadDir(directories[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			} else if file.Name() == command {
+				return directories[i] + "/" + file.Name()
+			}
+		}
+
+	}
+
+	return ""
+}
+
 func execREPL(allowed_prompts []string) {
 	var command string
 	var args []string
@@ -45,10 +70,16 @@ func execREPL(allowed_prompts []string) {
 			} else if command == "echo" {
 				fmt.Print(strings.Join(args, " "), "\n")
 			} else if command == "type" {
+
 				if len(args) > 0 && isValidCommand(args[0], allowed_prompts) {
 					fmt.Printf("%s is a shell builtin\n", args[0])
 				} else {
-					fmt.Printf("%s: not found\n", args[0])
+					found := isInPath(args[0])
+					if found != "" {
+						fmt.Printf("%s is %s\n", args[0], found)
+					} else {
+						fmt.Printf("%s: not found\n", args[0])
+					}
 				}
 			}
 		} else {
